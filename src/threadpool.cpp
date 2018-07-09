19ecc37a67c65fb7b440a448e4553579ea8eb91a
lunarwaterfox <lunarwaterfox@gmail.com>
2018-07-10 02:42:24 +0800

    add thread control

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -3,14 +3,26 @@
 
 namespace vuples {
     threadpool::threadpool(unsigned const thread_count) {
-
+        threads.reserve(thread_count);
+        for (int i = 0; i < thread_count; ++i) {
+            thread td(threadpool::work_thread, this);
+            threads.push_back(move(td));
+        }
     }
 
     threadpool::~threadpool() {
         
+        join_all();
     }
 
-    void add_task(vuples::task& task) {
+    void threadpool::add_task(vuples::task& task) {
 
+    }
+    
+    void threadpool::work_thread(threadpool* pool) {
+        
+    }
+    
+    void threadpool::join_all() {
     }
 }
--- src/threadpool.hpp
+++ src/threadpool.hpp
@@ -1,11 +1,20 @@
 #include "task.hpp"
 
+#include <vector>
+#include <thread>
+
+using namespace std;
+
 namespace vuples {
     class threadpool {
     public:
         threadpool(unsigned const thread_count = 1);
         ~threadpool();
         
-        void add_task(vuples::task& task); 
+        void add_task(vuples::task& task);
+    private:
+        vector<thread> threads;
+        static void work_thread(threadpool* pool);
+        void join_all();
     };
 }
